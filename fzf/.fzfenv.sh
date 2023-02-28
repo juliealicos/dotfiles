6848b4818ec2af5b5e2a7d86fafe71e541059bf7
Carlo Gravador <cce.gravador@gmail.com>
2023-02-28 18:41:57 +0800

    restructure

---
+++ fzf/.fzfenv.sh
@@ -0,0 +1,6 @@
+# Dracula theme for fzf
+export FZF_DEFAULT_OPTS="--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9"
+export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color=fg+:#f8f8f2,bg+:#282a36,hl+:#bd93f9"
+export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6"
+export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color=border:#6271A4"
--- nvim/README.md
+++ nvim/README.md
@@ -1,5 +1,16 @@
-Dependencies
+# Nvim lua config
+
+## Dependencies
     * Packer - Plugin Package manager
     * FZF - for fuzzy search (latest: 0.38 at moment of writing)
+    * bat - better preview for FZF
     * Node - for treesitter language installation
     * tree-sitter-cli - better syntax highlighting
+
+## Installation
+Once dependencies are installed, run the following commands in nvim command Mode
+```
+:PackCompile
+:PackInstall
+```
+
