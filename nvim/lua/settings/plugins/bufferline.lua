b0d5b4bf1c4fff2f385c1e63c87c6306ac32fb09
Carlo Gravador <cce.gravador@gmail.com>
2024-05-26 11:30:27 +0200

    Fix error on bufferline after upgrading to nvim 0.10

--- nvim/lua/settings/plugins/bufferline.lua
+++ nvim/lua/settings/plugins/bufferline.lua
@@ -1,7 +1,9 @@
 return {
     "akinsho/bufferline.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    version = "*",
+    -- version = "*",
+    -- Fix after upgrading to nvim 0.10
+    version = "73540cb95f8d95aa1af3ed57713c6720c78af915",
     opts = {
         options = {
             mode = "tabs",
