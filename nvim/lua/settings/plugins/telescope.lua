7671e8a6a5e533bf65262cc2aefc9b923aad7f33
Carlo Gravador <cce.gravador@gmail.com>
2025-01-03 20:57:52 +0100

    Add shortcuts for telescope functionalities

--- nvim/lua/settings/plugins/telescope.lua
+++ nvim/lua/settings/plugins/telescope.lua
@@ -1,3 +1,55 @@
+local state = require("telescope.state")
+local action_state = require("telescope.actions.state")
+
+local slow_scroll = function(prompt_bufnr, direction)
+    local previewer = action_state.get_current_picker(prompt_bufnr).previewer
+    local status = state.get_status(prompt_bufnr)
+
+    -- Check if we actually have a previewer and a preview window
+    if type(previewer) ~= "table" or previewer.scroll_fn == nil or status.preview_win == nil then
+        return
+    end
+
+    previewer:scroll_fn(1 * direction)
+end
+
+
+vim.api.nvim_create_user_command(
+    'RgDirs',
+    function(opts)
+        local dirs = opts.fargs -- fargs contains all arguments as a list
+        require('telescope.builtin').live_grep({ search_dirs = dirs})
+    end,
+    {
+        nargs = "*", -- Accept zero or more arguments
+        complete = "dir", -- Auto-complete directories
+    }
+)
+
+vim.api.nvim_create_user_command(
+    'RgRawDirs',
+    function(opts)
+        local dirs = opts.fargs -- fargs contains all arguments as a list
+        require("telescope").extensions.live_grep_args.live_grep_args({ search_dirs = dirs })
+    end,
+    {
+        nargs = "*", -- Accept zero or more arguments
+        complete = "dir", -- Auto-complete directories
+    }
+)
+
+vim.api.nvim_create_user_command(
+    'FindFilesDirs',
+    function(opts)
+        local dirs = opts.fargs -- fargs contains all arguments as a list
+        require("telescope.builtin").find_files({ search_dirs = dirs })
+    end,
+    {
+        nargs = "*", -- Accept zero or more arguments
+        complete = "dir", -- Auto-complete directories
+    }
+)
+
 return {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
@@ -6,6 +58,12 @@ return {
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
         "nvim-tree/nvim-web-devicons",
         'LukasPietzschmann/telescope-tabs',
+        {
+            "nvim-telescope/telescope-live-grep-args.nvim" ,
+            -- This will not install any breaking changes.
+            -- For major updates, this must be adjusted manually.
+            version = "^1.0.0",
+        },
     },
     config = function()
         local telescope = require("telescope")
@@ -30,16 +88,21 @@ return {
                         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to qflist and open
                         ["<C-a>"] = actions.select_all, -- send selected to qflist and open
                         -- vim.keymap.set("i", "<Esc>", "<C-c>", { desc = "Exit telescope" })
+                        ["<C-e>"] = function(bufnr) slow_scroll(bufnr, 1) end,
+                        ["<C-y>"] = function(bufnr) slow_scroll(bufnr, -1) end,
+                        ["<C-space>"] = actions.to_fuzzy_refine,
                     },
                 },
             },
         })
 
         telescope.load_extension("fzf")
+        telescope.load_extension("live_grep_args")
 
         -- set keymaps
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fF', ":FindFilesDirs<Space>")
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>ft', telescope_tabs.list_tabs, {})
@@ -47,7 +110,9 @@ return {
 
         -- grep
         vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>gd', ":RgDirs<Space>")
         vim.keymap.set("n", "<leader>gw", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+        vim.keymap.set("n", "<leader>gR", ":RgRawDirs<Space>")
 
         -- keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
         -- keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
